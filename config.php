e862d6beb21d99587016b34ba69e88b5576ead36
Aiwey <zielonkae989@gmail.com>
2020-09-27 22:11:32 +0200

    pls work

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $conf = new Config();
 $conf->root_path = dirname(__FILE__);
 $conf->server_name = 'localhost:80';
 $conf->server_url = 'http://'.$conf->server_name;
-$conf->app_root = '/PHP07';
+$conf->app_root = '/FrontController';
 $conf->app_url = $conf->server_url.$conf->app_root;
 
 $conf->action_root = $conf->app_root.'/app/ctrl.php?action=';
